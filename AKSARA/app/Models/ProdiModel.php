6b67e8726875400278c99c7686e0aff746d25adb
Ghetsa Ramadhani <ghetsa.arryanti@gmail.com>
2025-05-10 17:25:27 +0700

    fix: prodimodel

--- AKSARA/app/Models/ProdiModel.php
+++ AKSARA/app/Models/ProdiModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ProgramStudiModel extends Model
+class ProdiModel extends Model
 {
     use HasFactory;
 
